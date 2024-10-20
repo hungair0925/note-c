0ebe9acc3feb143b55c1afa40a6fbdbde9bf8ee2
hungair0925 <hungair0925@gmail.com>
2024-10-21 01:40:29 +0900

    fix: test

--- src/hello_struct.c
+++ src/hello_struct.c
@@ -8,7 +8,7 @@ typedef struct {
 
 int main() {
   Item item_list[ITEM_LIST_NUMBER] = {
-      {100, "apple"}, {180000, "Mac"}, {4500, "window"}};
+      {100, "apple"}, {180000, "Mac"}, {45000, "window"}};
 
   for (int i = 0; i < ITEM_LIST_NUMBER; i++) {
     printf("name: %-10s price: %d\n", item_list[i].name, item_list[i].price);
